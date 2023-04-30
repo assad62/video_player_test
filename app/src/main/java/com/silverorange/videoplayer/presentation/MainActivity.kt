e1bb2ee3640da7a7f76dea4b965891b7e9284b94
Mohammad Assad <assad62@hotmail.com>
2023-04-29 23:09:36 -0400

    changed folder structure to clean architecture format

--- app/src/main/java/com/silverorange/videoplayer/presentation/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.silverorange.videoplayer
+package com.silverorange.videoplayer.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
