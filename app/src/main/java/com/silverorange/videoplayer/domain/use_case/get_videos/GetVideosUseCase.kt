af4885bef26814335b79e0e81f7989522ac5c867
Mohammad Assad <assad62@hotmail.com>
2023-05-01 01:19:11 -0400

    sort api response by date, updated usecase

--- app/src/main/java/com/silverorange/videoplayer/domain/use_case/get_videos/GetVideosUseCase.kt
+++ app/src/main/java/com/silverorange/videoplayer/domain/use_case/get_videos/GetVideosUseCase.kt
@@ -7,15 +7,24 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class GetVideosUseCase @Inject constructor(
     private val repository: VideosRepository
 ) {
+
+    private fun sortByDate(videoList:List<Video>):List<Video>{
+        val format: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
+        return videoList.sortedByDescending { format.parse(it.publishedAt) }
+    }
+
     operator fun invoke(): Flow<Resource<List<Video>>> = flow {
         try {
             emit(Resource.Loading())
-            val videoList = repository.getVideos()
+            val videoList = sortByDate(repository.getVideos())
             emit(Resource.Success(videoList))
         } catch(e: HttpException) {
             emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
